404fa9fb5f4e04d2781fbe180c987aef3983d585
Mauricio Arenas <35950276+MicroCloud12@users.noreply.github.com>
2025-08-24 13:09:26 -0600

    Se adapto el header a moviles

--- finanzas/forms.py
+++ finanzas/forms.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import registro_transacciones, inversiones, Deuda, PagoAmortizacion
 
-
 class TransaccionesForm(forms.ModelForm):
     class Meta:
         model = registro_transacciones
