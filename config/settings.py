b2ad6675bf31c6e58ccde39723f199cdfb0508e3
Mauricio Arenas <infinitecode.consulting@gmail.com>
2026-06-10 11:00:12 -0600

    feat: initialize Django project settings with database, authentication, static storage, and logging configuration

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,9 @@ NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False").lower() == "true"
+
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(',')
 
 
 # Application definition
